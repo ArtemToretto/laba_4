ebfff8a32b42b716bd209cfa4924f85cee77b92e
Артём Чернов <artem_chernow@mail.ru>
2021-11-06 21:41:26 +0800

    Создал главный класс папу и классы детишки

--- Form1.cs
+++ Form1.cs
@@ -17,5 +17,9 @@ namespace laba_4
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
---
+++ drink.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace laba_4
+{
+    public class Drink
+    {
+        double size = 0;
+    }
+    public class Juice: Drink
+    {
+
+    }
+    public class Soda: Drink 
+    {
+
+    }
+    public class Alco: Drink
+    {
+
+    }
+}
