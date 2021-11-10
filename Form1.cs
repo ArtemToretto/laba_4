21906a6b1d2a4b6dbdf60c9f1ace835bbd24cfb4
Артём Чернов <artem_chernow@mail.ru>
2021-11-10 12:50:05 +0800

    Создал список drinks

--- Form1.cs
+++ Form1.cs
@@ -12,14 +12,14 @@ namespace laba_4
 {
     public partial class Form1 : Form
     {
+        List<Drink> drinks = new List<Drink>();
         public Form1()
         {
             InitializeComponent();
         }
-
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+
         }
     }
 }
--- drink.cs
+++ drink.cs
@@ -6,18 +6,24 @@ namespace laba_4
 {
     public class Drink
     {
-        double size = 0;
+        double size = 2;
     }
     public class Juice: Drink
     {
-
+        JuiceFruit fruit = JuiceFruit.apple;
+        bool pulp = false;
     }
     public class Soda: Drink 
     {
-
+        SodaType type = SodaType.CocaCola;
+        int bubbles = 10000;
     }
     public class Alco: Drink
     {
-
+        AlcoType type = AlcoType.Beer;
+        double alcoPercent = 10;
     }
+    public enum JuiceFruit { apple,orange,banana,tomato }
+    public enum SodaType { CocaCola,Sprite,Fanta,Pepsi }
+    public enum AlcoType { Beer,Vodka,Wine }
 }
